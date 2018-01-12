87583eff4fda9c8b66b6129df47033f95cc2f6bf
Ryan McLean <pvtryan100@gmail.com>
2018-01-12 20:05:29 +0000

    Reset Keycodes for working with backlight

--- keyboards/jj40/keymaps/ryan/keymap.c
+++ keyboards/jj40/keymaps/ryan/keymap.c
@@ -1,20 +1,3 @@
-/*
-Copyright 2017 Luiz Ribeiro <luizribeiro@gmail.com>
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
 #include "jj40.h"
 #include "action_layer.h"
 
@@ -62,16 +45,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LOWER] = KEYMAP( \
    KC_GRV,  KC_INSERT,   KC_PGUP,           KC_HOME,    KC_RBRC, _______,  KC_LPRN,    KC_7,    KC_8,              KC_9,   KC_RPRN, _______, \
-  KC_CAPSLOCK,  KC_DELETE, KC_PGDOWN,            KC_END,    KC_LBRC, _______,   KC_EQL,    KC_4,    KC_5,              KC_6,   S(KC_8), _______, \
+  _______,  KC_DELETE, KC_PGDOWN,            KC_END,    KC_LBRC, _______,   KC_EQL,    KC_4,    KC_5,              KC_6,   S(KC_8), _______, \
   KC_LSFT, KC_PSCREEN,   _______,           _______, TT(_NINJA), _______,  KC_MINS,    KC_1,    KC_2,              KC_3,   KC_SLSH,  KC_ENT, \
   KC_LCTL,    KC_LGUI,   KC_LALT,           KC_NUHS,    _______,      KC_SPC      , _______,    KC_0,    ALGR_T(KC_DOT), _______, KC_MENU \
 ),
 
 /* NINJA
  * ,-----------------------------------------------------------------------------------.
- * |RGBHUD|RGBHUI|RGBSAD|RGBSAD|      |      |      | PGDN | PGUP |      |      |      |
+ * |RGBTOG|RGBHUI|RGBHUD|RGBSAI|RGBSAD|      |      | PGDN | PGUP |      |      |      |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |      |      |      |      |      | Left | Down |  Up  | Right|      |      |
+ * |BLTOG |BLSTEP|RGBVAI|RGBVAD|RGBMOD|RGBRMD| Left | Down |  Up  | Right|      |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |      |      |      |      |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -79,10 +62,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
  [_NINJA] = KEYMAP( \
-    RGB_HUD, RGB_HUI, RGB_SAD, RGB_SAI, RGB_TOG, _______, _______,   KC_PGDOWN, KC_PGUP,  _______, _______, _______, \
-    BL_TOGG, BL_ON, BL_OFF, BL_INC, _______, _______, KC_LEFT,     KC_DOWN,   KC_UP, KC_RIGHT, _______, _______, \
-    _______, _______, _______, _______, _______, _______, _______,     _______, _______,  _______, _______,  KC_ENT, \
-    _______, _______, _______, _______, _______,      KC_SPC     , TO(_QWERTY), _______,  _______, _______, _______ \
+    RGB_TOG, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD,  _______, _______,   KC_PGDOWN, KC_PGUP,  _______, _______, _______, \
+    BL_TOGG, BL_STEP, RGB_VAI, RGB_VAD, RGB_MOD, RGB_RMOD, KC_LEFT,     KC_DOWN,   KC_UP, KC_RIGHT, _______, _______, \
+    _______, _______, _______, _______, _______,  _______, _______,     _______, _______,  _______, _______,  KC_ENT, \
+    _______, _______, _______, _______, _______,       KC_SPC     , TO(_QWERTY), _______,  _______, _______, _______ \
  ),
 
 /* Raise
