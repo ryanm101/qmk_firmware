b2d877bf5b53eebdd441ba98217c02b632d2a4e8
Ryan McLean <ryan1_00@hotmail.com>
2018-02-14 16:24:07 +0000

    added MAC support

--- keyboards/jj40/keymaps/rytap/keymap.c
+++ keyboards/jj40/keymaps/rytap/keymap.c
@@ -5,9 +5,10 @@
 #define XXXXXXX KC_NO
 
 #define _QWERTY 0
-#define _L1  1
-#define _L2  2
-#define _L3  3
+#define _QWERTYMAC 1
+#define _L1  2
+#define _L2  3
+#define _L3  4
 
 // Custom Key Combos
 #define KC_CAD LCTL(LALT(KC_DEL)) // CTL+ALT+DEL (windows)
@@ -15,6 +16,7 @@
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
+  QWERTYMAC,
   L1,
   L2,
   L3
@@ -60,6 +62,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_LCTL, KC_LGUI, KC_LALT, KC_NONUS_BSLASH,    MO(_L1),     KC_SPC    ,    MO(_L2), KC_LEFT, KC_DOWN,       KC_UP,  KC_RGHT \
 ),
 
+/* QwertyMAC
+ * ,-----------------------------------------------------------------------------------.
+ * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * | Esc  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |  ;:  |  '@  |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |  ,<  |  .>  |  /?  |Enter |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Ctrl | Alt  | GUI  |  \|  |  L1  |    Space    |  L2  | Left | Down |  Up  |Right |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_QWERTYMAC] = KEYMAP( \
+   KC_TAB,    KC_Q, KC_W,               KC_E,       KC_R,  KC_T,    KC_Y,       KC_U,    KC_I,    KC_O,        KC_P,  KC_BSPC, \
+   KC_ESC,    KC_A, KC_S,               KC_D,       KC_F,  KC_G,    KC_H,       KC_J,    KC_K,    KC_L,       COLON,  QUOTE, \
+  KC_LSFT,    KC_Z, KC_X,               KC_C,       KC_V,  KC_B,    KC_N,       KC_M, KC_COMM,  KC_DOT,     KC_SLSH,   KC_ENT, \
+  KC_LCTL, KC_LALT, KC_LGUI, KC_NONUS_BSLASH,    MO(_L1),     KC_SPC    ,    MO(_L2), KC_LEFT, KC_DOWN,       KC_UP,  KC_RGHT \
+),
+
 /* L1
  * ,-----------------------------------------------------------------------------------.
  * |  `¬  |INSERT| PGUP | HOME |  <>  |      |  ()  |  7&  |  8*  |  9(  |      |      |
@@ -72,7 +92,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_L1] = KEYMAP( \
-   KC_GRV,  KC_INSERT,   KC_PGUP,  KC_HOME,   ANGUL, XXXXXXX,   PARAN,    KC_7,  KC_8,           KC_9, XXXXXXX, XXXXXXX, \
+   KC_GRV,  KC_INSERT,   KC_PGUP,  KC_HOME,   ANGUL, XXXXXXX,   PARAN,    KC_7,  KC_8,           KC_9, XXXXXXX, TG(_QWERTYMAC), \
   XXXXXXX,  KC_DELETE, KC_PGDOWN,   KC_END,   SQUAR,   CURLY,  KC_EQL,    KC_4,  KC_5,           KC_6, S(KC_8), XXXXXXX, \
   KC_LSFT, KC_PSCREEN,   XXXXXXX,   CADCAE, MO(_L3), XXXXXXX, KC_MINS,    KC_1,  KC_2,           KC_3, KC_SLSH,  KC_ENT, \
   KC_LCTL,    KC_LGUI,   KC_LALT,  KC_NUHS, XXXXXXX,      KC_SPC     , XXXXXXX,  KC_0, ALGR_T(KC_DOT), XXXXXXX, KC_MENU \
