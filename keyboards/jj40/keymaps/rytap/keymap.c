95fe3d2a1119e548af70eb6f74c981cfe225c305
Mclean <ryan1_00@hotmail.com>
2018-02-14 22:03:25 +0000

    Added more MAC commands

--- keyboards/jj40/keymaps/rytap/keymap.c
+++ keyboards/jj40/keymaps/rytap/keymap.c
@@ -4,15 +4,16 @@
 #define _______ KC_TRNS
 #define XXXXXXX KC_NO
 
+// Custom Key Combos
+//#define KC_CAD LCTL(LALT(KC_DEL)) // CTL+ALT+DEL (windows)
+//#define KC_CAE LGUI(LALT(KC_ESC)) // CMD+ALT+ESC (Force Close)
+//#define KC_SCREENSHOT LGUI(S(KC_4)) // CMD+SHIFT+4 (Mac Screenshot)
+
 #define _QWERTY 0
 #define _QWERTYMAC 1
 #define _L1  2
 #define _L2  3
 #define _L3  4
-
-// Custom Key Combos
-#define KC_CAD LCTL(LALT(KC_DEL)) // CTL+ALT+DEL (windows)
-#define KC_CAE LGUI(LALT(KC_ESC)) // CMD+ALT+ESC (Force Close)
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
@@ -82,9 +83,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* L1
  * ,-----------------------------------------------------------------------------------.
- * |  `¬  |INSERT| PGUP | HOME |  <>  |      |  ()  |  7&  |  8*  |  9(  |      |      |
+ * |  `¬  |INSERT| PGUP | HOME |  <>  |      |  ()  |  7&  |  8*  |  9(  |      | Bksp |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |DELETE| PGDN | END  |  []  |  {}  |  =+  |  4$  |  5%  |  6^  |  *   |      |
+ * |      |DELETE| PGDN | END  |  []  |  {}  |  =+  |  4$  |  5%  |  6^  |  *   | LMAC |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |LSHIFT|PSCRN |      |CADCAE|  L3  |      |  -_  |  1!  |  2"  |  3£  |  /?  |ENTER |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -92,10 +93,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_L1] = KEYMAP( \
-   KC_GRV,  KC_INSERT,   KC_PGUP,  KC_HOME,   ANGUL, XXXXXXX,   PARAN,    KC_7,  KC_8,           KC_9, XXXXXXX, TG(_QWERTYMAC), \
-  XXXXXXX,  KC_DELETE, KC_PGDOWN,   KC_END,   SQUAR,   CURLY,  KC_EQL,    KC_4,  KC_5,           KC_6, S(KC_8), XXXXXXX, \
-  KC_LSFT, KC_PSCREEN,   XXXXXXX,   CADCAE, MO(_L3), XXXXXXX, KC_MINS,    KC_1,  KC_2,           KC_3, KC_SLSH,  KC_ENT, \
-  KC_LCTL,    KC_LGUI,   KC_LALT,  KC_NUHS, XXXXXXX,      KC_SPC     , XXXXXXX,  KC_0, ALGR_T(KC_DOT), XXXXXXX, KC_MENU \
+   KC_GRV,  KC_INSERT,   KC_PGUP,  KC_HOME,   ANGUL, XXXXXXX,   PARAN,    KC_7,  KC_8,           KC_9, XXXXXXX, KC_BSPC, \
+  XXXXXXX,  KC_DELETE, KC_PGDOWN,   KC_END,   SQUAR,   CURLY,  KC_EQL,    KC_4,  KC_5,           KC_6, S(KC_8), TG(_QWERTYMAC), \
+  _______, KC_PSCREEN,   XXXXXXX,   CADCAE, MO(_L3), XXXXXXX, KC_MINS,    KC_1,  KC_2,           KC_3, KC_SLSH,  KC_ENT, \
+  _______,    _______,   _______,  KC_NUHS, XXXXXXX,      KC_SPC     , XXXXXXX,  KC_0, ALGR_T(KC_DOT), XXXXXXX, KC_MENU \
 ),
 
 /* L2
@@ -113,7 +114,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_SYSREQ,   KC_F9,  KC_F10,  KC_F11,      KC_F12, XXXXXXX, KC_NUMLOCK, KC_KP_7, KC_KP_8,   KC_KP_9, KC_KP_ASTERISK,     KC_BSPC, \
         XXXXXXX,   KC_F5,   KC_F6,   KC_F7,       KC_F8, XXXXXXX,    XXXXXXX, KC_KP_4, KC_KP_5,   KC_KP_6,    KC_KP_SLASH,     _______, \
         XXXXXXX,   KC_F1,   KC_F2,   KC_F3,       KC_F4, XXXXXXX,    XXXXXXX, KC_KP_1, KC_KP_2,   KC_KP_3,     KC_KP_PLUS, KC_KP_ENTER, \
-        KC_LCTL, KC_LGUI, KC_LALT, XXXXXXX,     XXXXXXX,        KC_SPC      , XXXXXXX, KC_KP_0, KC_KP_DOT,    KC_KP_MINUS,      _______ \
+        _______, _______, _______, XXXXXXX,     XXXXXXX,        KC_SPC      , XXXXXXX, KC_KP_0, KC_KP_DOT,    KC_KP_MINUS,      _______ \
  ),
 
 /* L3
@@ -189,11 +190,36 @@ void tmux_dance (qk_tap_dance_state_t *state, void *user_data) {
   }
 }
 
+void cmd_dance (qk_tap_dance_state_t *state, void *user_data) {
+  if (state->count == 1) {
+    register_mods(MOD_BIT(KC_LCTRL));
+    register_mods(MOD_BIT(KC_LALT));
+    register_code(KC_DELETE);
+    unregister_mods(MOD_BIT(KC_LCTRL));
+    unregister_mods(MOD_BIT(KC_LALT));
+    unregister_code(KC_DELETE);
+  } else if (state->count == 2) {
+    register_mods(MOD_BIT(KC_LGUI));
+    register_mods(MOD_BIT(KC_LALT));
+    register_code(KC_ESC);
+    unregister_mods(MOD_BIT(KC_LGUI));
+    unregister_mods(MOD_BIT(KC_LALT));
+    unregister_code(KC_ESC);
+  } else if (state->count == 3) {
+    register_mods(MOD_BIT(KC_LGUI));
+    register_mods(MOD_BIT(KC_LSHIFT));
+    register_code(KC_4);
+    unregister_mods(MOD_BIT(KC_LGUI));
+    unregister_mods(MOD_BIT(KC_LSHIFT));
+    unregister_code(KC_4);
+  }
+} 
+
  //All tap dance functions would go here. Only showing this one.
  qk_tap_dance_action_t tap_dance_actions[] = {
    [CLN] = ACTION_TAP_DANCE_DOUBLE (KC_SCLN, S(KC_SCLN ))
    ,[QUOT] = ACTION_TAP_DANCE_DOUBLE (KC_QUOT, S(KC_2))
-   ,[CAD_CAE] = ACTION_TAP_DANCE_DOUBLE (KC_CAD, KC_CAE)
+   ,[CAD_CAE] = ACTION_TAP_DANCE_FN_ADVANCED( NULL, NULL, cmd_dance )
    ,[PAR] = ACTION_TAP_DANCE_FN_ADVANCED( NULL, NULL, paranthesis_dance )
    ,[CUR] = ACTION_TAP_DANCE_FN_ADVANCED( NULL, NULL, curly_dance )
    ,[SQU] = ACTION_TAP_DANCE_FN_ADVANCED( NULL, NULL, square_dance )
