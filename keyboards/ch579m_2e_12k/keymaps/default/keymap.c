bba8f9a11a0f27b321e2f8e74538e164a0117a3e
Ryan McLean <pvtryan100@gmail.com>
2026-03-21 17:06:26 +0000

    Fix Linting

--- keyboards/ch579m_2e_12k/keymaps/default/keymap.c
+++ keyboards/ch579m_2e_12k/keymaps/default/keymap.c
@@ -1,3 +1,18 @@
+/* Copyright 2026 Ryan McLean
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #include QMK_KEYBOARD_H
 
 /*
@@ -9,10 +24,14 @@
  *  r1   KC_F / B20  KC_G / A12  KC_H / A3
  *  r2   KC_I / B22  KC_J / A13  KC_K / A2
  *  r3   KC_L / B15  KC_M / A14  KC_N / A1
+ *
+ * Layer 1 (hold ENC1 button):
+ *   ENC2 button = QK_BOOT (reset/reflash)
  */
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
-        KC_A,
+        MO(1),
         KC_C,  KC_D,
         KC_F,
         KC_I,
@@ -23,6 +42,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_B,
         KC_G,
         KC_E,  KC_H
+    ),
+    [1] = LAYOUT(
+        _______,
+        _______, _______,
+        _______,
+        _______,
+        _______,
+        _______,
+        _______,
+        _______, _______,
+        QK_BOOT,
+        _______,
+        _______, _______
     )
 };
 
