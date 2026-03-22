bf545d9f5875619ebf6ecacb920323494a9810ff
Ryan McLean <pvtryan100@gmail.com>
2026-03-22 07:44:06 +0000

    Fix Linting

--- keyboards/ch579m_2e_12k/rules.mk
+++ keyboards/ch579m_2e_12k/rules.mk
@@ -13,10 +13,7 @@ BOOTMAGIC_ENABLE = yes  # hold ENC1 button at power-on to reset
 MOUSEKEY_ENABLE  = no
 EXTRAKEY_ENABLE  = yes  # needed for KC_VOLU/VOLD consumer codes
 NKRO_ENABLE      = no   # adds USB descriptors — disable until USB baseline works
-ENCODER_ENABLE      = yes
-ENCODER_MAP_ENABLE  = yes
-
-CONSOLE_ENABLE   = no
+ENCODER_ENABLE   = yes
 KEYBOARD_SHARED_EP = no
 CUSTOM_MATRIX    = lite
 SRC             += matrix.c hooks.c
